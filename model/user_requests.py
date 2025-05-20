ae2d5029ec3396086ad2d0d5852d559dc71fb3e7
AbdoEssam2024 <eabdo2051@gmail.com>
2025-05-21 00:57:19 +0300

    upload backend

---
+++ model/user_requests.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+class LoginRequest(BaseModel):
+    email: str
+    password: str
+
+
+class RegisterRequest(BaseModel):
+    email: str
+    password: str
+    username: str
+
+
+class UpdateRequest(BaseModel):
+    username: str | None = None
+    password: str | None = None
+    email: str | None = None
+
+
+class DeleteRequest(BaseModel):
+    user_id: int
