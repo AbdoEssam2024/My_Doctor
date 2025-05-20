5f6e4cc8d5352a40a1f74200e99167297392ac3d
AbdoEssam2024 <eabdo2051@gmail.com>
2025-05-21 01:13:08 +0300

    edit main

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
 from fastapi import FastAPI
 from routes.user_routes import router as user_router
 
-import secrets
-
 app = FastAPI()
-
-SECRET_KEY = secrets.token_hex(32)  # يولّد مفتاح بطول 64 حرف (32 بايت)
-print(SECRET_KEY)
-
-
-
 
 app.include_router(
     router=user_router,
